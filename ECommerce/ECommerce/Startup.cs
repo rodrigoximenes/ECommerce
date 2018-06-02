2b20c2a64b4ce69fcde08bc27cb1c132a7b08d2a
Rodrigo <Rodrigo>
2018-06-01 21:32:12 -0300

    ActionLink

--- ECommerce/ECommerce/Startup.cs
+++ ECommerce/ECommerce/Startup.cs
@@ -43,7 +43,7 @@ namespace ECommerce
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Pedido}/{action=Carrossel}/{id?}");
             });
         }
     }
