e5189606fe27d36397c1248c56e8c258bd1f35ae
Rodrigo <Rodrigo>
2018-06-04 21:17:49 -0300

    Entity

---
+++ ECommerce/ECommerce/Infrastructure/Contexto.cs
@@ -0,0 +1,15 @@
+﻿using ECommerce.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECommerce.Infrastructure
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Produto> ProdutosContext{ get; set; }
+        public DbSet<ItemPedido> ItemPedidoContext { get; set; }
+    }
+}
--- ECommerce/ECommerce/Startup.cs
+++ ECommerce/ECommerce/Startup.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ECommerce.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace ECommerce
 {
@@ -22,6 +20,8 @@ namespace ECommerce
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ECommerce;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            services.AddDbContext<Contexto>(x => x.UseSqlServer(connectionString));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
